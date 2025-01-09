8bc093f933c96984490e142c614c337c99dd0b6d
seunghyun <tmdgus7048@naver.com>
2025-01-09 16:56:13 +0900

    feat: banner 추가

--- s-blog/src/app/page.tsx
+++ s-blog/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Banner from "@/components/Banner";
 import CarouselPosts from "@/components/CarouselPosts";
 import MainPosts from "@/components/MainPosts";
 
 export default function HomePage() {
   return (
-    <div className="w-full max-w-screen-lg mx-auto">
+    <div className="flex flex-col gap-8">
+      <Banner />
       <MainPosts />
       <CarouselPosts />
     </div>
