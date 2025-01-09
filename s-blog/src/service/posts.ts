a78b0bb6b244807e7b8c6975e488b61be00efa17
seunghyun <tmdgus7048@naver.com>
2025-01-09 17:14:02 +0900

    feat: 포스트 데이터 조회 기능 구현

--- s-blog/src/service/posts.ts
+++ s-blog/src/service/posts.ts
@@ -11,6 +11,10 @@ export type Post = {
   mainPost: boolean;
 };
 
+export type PostData = Post & {
+  content: string;
+};
+
 export async function getMainPosts(): Promise<Post[]> {
   return getAllPosts().then((posts) => posts.filter((post) => post.mainPost));
 }
@@ -23,4 +27,14 @@ export async function getAllPosts(): Promise<Post[]> {
   return readFile(filePath, "utf-8")
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
+  const metadata = await getAllPosts().then((posts) =>
+    posts.find((post) => post.path === fileName)
+  );
+  if (!metadata) throw new Error(`${fileName} 포스트를 찾을 수 없습니다.`);
+
+  const content = await readFile(filePath, "utf-8");
+  return { ...metadata, content };
 }
