20236d8c7e2c6fe15447ac2a021df3e894965649
seunghyun <tmdgus7048@naver.com>
2025-01-09 17:06:04 +0900

    feat: 포스트 상세 페이지 레이아웃 추가

---
+++ s-blog/src/app/posts/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import MarkdownViewer from "@/components/MarkdownViewer";
+import { getPostData } from "@/service/posts";
+import Image from "next/image";
+
+interface ParamsProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function PostPage({ params }: ParamsProps) {
+  const { slug } = await Promise.resolve(params);
+  const post = await getPostData(slug);
+
+  return (
+    <article>
+      <h1 className="text-4xl font-bold text-white mb-2">{post.title}</h1>
+      <p className="text-white/[0.5] mb-10">{post.date}</p>
+      <div className="w-full h-[600px] px-28 py-20 border-b-[1px] border-white/[0.1]">
+        <Image
+          className="w-full h-full"
+          src={`/images/posts/${post.path}.png`}
+          alt={post.title}
+          width={800}
+          height={600}
+        />
+      </div>
+      <MarkdownViewer content={post.content} />
+    </article>
+  );
+}
