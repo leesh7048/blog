4a97fcd54805bd914f6693bd9954d03c2ed23c1e
seunghyun <tmdgus7048@naver.com>
2025-01-09 17:10:04 +0900

    feat: 마크다운 뷰어 컴포넌트 추가

---
+++ s-blog/src/components/MarkdownViewer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+export default function MarkdownViewer({ content }: { content: string }) {
+  return (
+    <ReactMarkdown className="prose " remarkPlugins={[remarkGfm]}>
+      {content}
+    </ReactMarkdown>
+  );
+}
