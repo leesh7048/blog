3c515090a05e587596e06583117a1deddab975da
seunghyun <tmdgus7048@naver.com>
2025-01-09 17:08:51 +0900

    feat: 메인페이지 메인포스트 스타일 수정

--- s-blog/src/components/MainPosts.tsx
+++ s-blog/src/components/MainPosts.tsx
@@ -4,8 +4,8 @@ import PostsGrid from "./PostsGrid";
 export default async function MainPosts() {
   const posts = await getMainPosts();
   return (
-    <section>
-      <h2 className="text-2xl font-bold">MainPosts</h2>
+    <section className="flex flex-col gap-6">
+      <h2 className="text-2xl text-white font-bold">MainPosts</h2>
       <PostsGrid posts={posts} />
     </section>
   );
