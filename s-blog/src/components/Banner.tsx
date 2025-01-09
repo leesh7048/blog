b6fa3bf3938ba6d89dfdce37ed5e29507a0b9788
seunghyun <tmdgus7048@naver.com>
2025-01-09 16:57:16 +0900

    feat: banner 컴포넌트 추가

---
+++ s-blog/src/components/Banner.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <div className="bg-[rgb(30,62,125)] py-32 mb-8">
+      <h2 className="text-4xl text-white font-bold text-center">
+        Frontend Developer Blog
+      </h2>
+      <p className="text-white/80 text-center mt-4">
+        Sharing frontend development insights and showcasing personal projects
+      </p>
+    </div>
+  );
+}
