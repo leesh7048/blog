aa2233b8f7fb1b1fcadbf691d7a1cc46c05c30ae
seunghyun <tmdgus7048@naver.com>
2025-01-09 16:56:59 +0900

    feat: footer 추가

--- s-blog/src/components/Footer.tsx
+++ s-blog/src/components/Footer.tsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-center items-center py-4">
+      <p className="text-sm text-white border-t-[1px] border-white/[0.1] pt-2 w-full text-center">
+        S-Blog
+      </p>
+    </div>
+  );
 }
